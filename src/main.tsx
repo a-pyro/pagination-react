a9e926d0042304b603d797f10fde81d5f5b8e7a8
Ardi Germenji <ardigermenji@MacBook-Air-di-Ardi.local>
2022-09-03 11:20:13 -0400

    feat(h): yattaaaa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log(
+  "%cA black belt is a white belt that didn't quit.",
+  'color: #fff; font-size: 20px; text-shadow: 1px 1px 1px #000; font-family: serif;'
+)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
